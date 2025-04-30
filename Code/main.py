b4a155abb607dbbeb1165c9eff0e893a80dbd562
Luuloneranger <luuga000@gmail.com>
2025-04-30 12:25:35 +0700

    Cập nhật chia lượt, xác định thắng thua, cơ chế nhập thành

--- Code/main.py
+++ Code/main.py
@@ -25,7 +25,19 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             QuanCo.DiChuyenCo(banco_matrix,event)
             # QuanCo.DiChuyenNgua(banco_matrix,event)
+            ket_qua = QuanCo.KiemTraSauNuocDi(banco_matrix, QuanCo.luot)
+            if ket_qua == 'thang':
+                QuanCo.HienThiThongBao(screen, "You Win!")
+                pygame.time.wait(3000)  # Hiển thị thông báo trong 3 giây
+                running = False  # Kết thúc trò chơi
+            elif ket_qua == 'thua':
+                QuanCo.HienThiThongBao(screen, "You Lose!")
+                pygame.time.wait(3000)  # Hiển thị thông báo trong 3 giây
+                running = False  # Kết thúc trò chơi
+            elif ket_qua == 'chieu':
+                print("Bạn đang bị chiếu! Hãy thoát khỏi chiếu.")
     banco.VeBanCo(QuanCo.nuoc_di_hop_le)
     banco.VeQuanCo(banco_matrix)
+
     pygame.display.flip()
 pygame.display.quit()
